cde03609bba8268a9c37595223e87adb1faabe7d
anwebdevelopers <anwebdev78@gmail.com>
2017-11-01 19:40:56 +0300

    1.11.2017

--- html/js/common.js
+++ html/js/common.js
@@ -23,7 +23,7 @@ $(function() {
 
     $headerButtonMenu.click(function() {
         $(this).toggleClass('active');
-        $headerBottomBox.slideToggle(200);
+        $headerBottomBox.stop().slideToggle(200);
     });
 
     $window.resize(function() {
@@ -34,6 +34,96 @@ $(function() {
         } else {
             $headerButtonMenu.removeClass('active');
             $headerBottomBox.removeAttr('style');
+        }
+    });
+
+    /*******************************************************/
+    //PRODUCTION SLIDER
+    /*******************************************************/
+    $('.production__box').addClass('owl-carousel').owlCarousel({
+        loop: true,
+        nav: true,
+        navText: '',
+        autoplay: true,
+        autoplayTimeout: 5000,
+        smartSpeed: 300,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            481: {
+                items: 2
+            },
+            993: {
+                items: 3
+            },
+            1201: {
+                items: 4
+            }
+        }
+    });
+
+    /*******************************************************/
+    //CLIENTS SLIDER
+    /*******************************************************/
+    $('.clients__list').addClass('owl-carousel').owlCarousel({
+        loop: true,
+        nav: true,
+        navText: '',
+        autoplay: true,
+        autoplayTimeout: 5000,
+        smartSpeed: 300,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            361: {
+                items: 2
+            },
+            993: {
+                items: 3
+            },
+            1201: {
+                items: 4
+            }
+        }
+    });
+
+    /*******************************************************/
+    //INSTAGRAM SLIDER
+    /*******************************************************/
+    $('.instagram__box').addClass('owl-carousel').owlCarousel({
+        loop: true,
+        nav: true,
+        navText: '',
+        autoplay: true,
+        autoplayTimeout: 5000,
+        smartSpeed: 300,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            481: {
+                items: 3
+            },
+            641: {
+                items: 4
+            },
+            769: {
+                items: 5
+            },
+            993: {
+                items: 6
+            },
+            1201: {
+                items: 7
+            },
+            1401: {
+                items: 8
+            }
         }
     });
 
