551f7c093ee28159bf05a6c6917196bb4f57baf3
anwebdevelopers <anwebdev78@gmail.com>
2017-11-06 16:39:30 +0300

    6.11.2017

--- html/js/common.js
+++ html/js/common.js
@@ -127,4 +127,38 @@ $(function() {
         }
     });
 
+    //-------------------------------
+    // Google Map
+    //-------------------------------
+    if(typeof google === 'object' && typeof google.maps === 'object' && $('#map').length) {
+        function initializeMap() {
+            var markerPositions = new Array();
+                markerPositions[1] = new google.maps.LatLng(47.222831, 39.715031);
+
+            var loc = new google.maps.LatLng(47.222831, 39.715031);
+
+            var map = new google.maps.Map(document.getElementById('map'), {
+                 zoom: 16,
+                 center: loc,
+                 mapTypeId: google.maps.MapTypeId.ROADMAP,
+                 scrollwheel: false
+            });
+
+            var iconlink = 'img/';
+            for(var i in markerPositions) {
+              var markerPosition = markerPositions[i];
+              var marker = new google.maps.Marker({
+                  map: map,
+                  position: markerPosition,
+                  visible: true,
+                  icon: iconlink + 'icon-map.png'
+              });
+            }
+
+            map.set('styles', [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ccc"},{"lightness":20},{"saturation":-97}]}]);
+        }
+        initializeMap();
+        google.maps.event.addDomListener(window, 'load', initializeMap);
+    }
+
 });
